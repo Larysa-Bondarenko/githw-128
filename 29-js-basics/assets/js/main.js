aed08672e46941b05c68fdb91cc1dc0a6d417013
Larysa Bondarenko <pani.lbondarenko@gmail.com>
2024-01-02 18:52:10 +0200

    added topic 29 JS Basics

---
+++ 29-js-basics/assets/js/main.js
@@ -0,0 +1,39 @@
+function task1(){
+ const userName = prompt('What is your name?');
+// alert('Hello, ' + userName + '!');
+alert(`Hello, ${userName}!`);
+}
+
+function task2(){
+    const birthYear = parseInt(prompt('Enter your birth year'));
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+    alert(`You are ${age} years old!`);   
+}
+
+function task3(){
+    const sideLength = parseInt(prompt('Enter side length of the square'));
+    const squarePerimeter = 4 * sideLength;
+    alert(`The square perimeter is ${squarePerimeter}.`);
+}
+
+function task4(){
+    const radius = parseInt(prompt('Enter circle radius'));
+    const area = Math.PI * Math.pow(radius, 2); //or r + r
+    alert(`The area of the circle is ${area}.`);
+}
+
+function task5(){
+    const distance = parseFloat(prompt('Enter the distance between two cities in km'));
+    const time = parseFloat(prompt('Enter the time (in hours) you need to get to the place of destination'));
+    const speed = distance / time;
+    alert(`You have to travel at ${speed} km per hour.`);
+}
+
+function task6(){
+    const dollar = parseFloat(prompt('Enter the ammount in US dollars'));
+    const exchangeRate = 0.91;
+    const euro = dollar * exchangeRate;
+    alert(` ${euro} Euro.`);
+}
+
