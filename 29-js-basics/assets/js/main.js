453ad75d93ed6ad6b87418cc7ef24f4c5593706a
Larysa Bondarenko <pani.lbondarenko@gmail.com>
2024-01-02 23:34:21 +0200

    added modified files to topic 29 JS Basics

--- 29-js-basics/assets/js/main.js
+++ 29-js-basics/assets/js/main.js
@@ -5,20 +5,20 @@ alert(`Hello, ${userName}!`);
 }
 
 function task2(){
-    const birthYear = parseInt(prompt('Enter your birth year'));
+    const birthYear = parseInt(prompt('Enter your birth year'), 10);
     const currentYear = new Date().getFullYear();
     const age = currentYear - birthYear;
     alert(`You are ${age} years old!`);   
 }
 
 function task3(){
-    const sideLength = parseInt(prompt('Enter side length of the square'));
+    const sideLength = parseInt(prompt('Enter side length of the square'), 10);
     const squarePerimeter = 4 * sideLength;
     alert(`The square perimeter is ${squarePerimeter}.`);
 }
 
 function task4(){
-    const radius = parseInt(prompt('Enter circle radius'));
+    const radius = parseInt(prompt('Enter circle radius'), 10);
     const area = Math.PI * Math.pow(radius, 2); //or r + r
     alert(`The area of the circle is ${area}.`);
 }
@@ -34,6 +34,7 @@ function task6(){
     const dollar = parseFloat(prompt('Enter the ammount in US dollars'));
     const exchangeRate = 0.91;
     const euro = dollar * exchangeRate;
-    alert(` ${euro} Euro.`);
+    const roundedeuro = Math.round(euro);
+    alert(` ${roundedeuro} Euro.`);
 }
 
